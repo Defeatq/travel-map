5afb7e096d9d4c9b3ba891feea2975cbb006cf23
Artem <awrer1234eads@gmail.com>
2022-07-12 23:01:12 +0300

    create list layout

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,8 +1,33 @@
+import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+
 function List() {
+  const [type, setType] = useState('');
+  const [rating, setRating] = useState('');
+
   return (
-    <h1>
-      List
-    </h1>
+    <div>
+      <Typography variant="h5">
+        Food & Dining around you
+      </Typography>
+      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel>Type</InputLabel>
+        <Select value={ type } onChange={ (event: SelectChangeEvent) => setType(event.target.value) } label="Type">
+          <MenuItem value={ 'restaurants' }>Restaurants</MenuItem>
+          <MenuItem value={ 'hotels' }>Hotels</MenuItem>
+          <MenuItem value={ 'attractions' }>Attractions</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel>Rating</InputLabel>
+        <Select value={ rating } onChange={ (event: SelectChangeEvent) => setRating(event.target.value) }>
+          <MenuItem value={ 0 }>All</MenuItem>
+          <MenuItem value={ 3 }>Above 3.0</MenuItem>
+          <MenuItem value={ 4 }>Above 4.0</MenuItem>
+          <MenuItem value={ 4.5 }>Above 4.5</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
   )
 }
 
