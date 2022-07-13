05c1718258d30a2992f8552ea5090299cddf1c7d
Artem <awrer1234eads@gmail.com>
2022-07-13 21:49:50 +0300

    rename map component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { CssBaseline, Grid } from '@mui/material';
 
 import Header from './components/Header/Header';
 import List from './components/List/List';
-import Map from './components/Map/Map';
+import OverviewMap from './components/Map/Map';
 
 function App() {
   return (
@@ -11,13 +11,13 @@ function App() {
 
       <Header />
 
-      <Grid container spacing={{ xs: 0, md: 4 }} sx={{ width: '100%' }}>
+      <Grid container spacing={ 3 } sx={{ width: '100%' }}>
         <Grid item xs={ 12 } md={ 4 }>
           <List />
         </Grid>
 
         <Grid item xs={ 12 } md={ 8 }>
-          <Map />
+          <OverviewMap />
         </Grid>
       </Grid>
     </>
