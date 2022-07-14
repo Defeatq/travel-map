7c3b6f37dea69dc025338b1d78104d453f096cc9
Artem <awrer1234eads@gmail.com>
2022-07-14 19:38:17 +0300

    add store based on map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,28 +1,40 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
-import Map, { LngLatBounds } from 'react-map-gl';
+import { useEffect, useRef } from 'react';
+import Map from 'react-map-gl';
 
 import token from '../../token'; // Mapbox token
-import useStyles from './MapStyle';
+import { setBounds } from '../../rtk/actions';
+import { RootState, AppDispatch } from '../../rtk/store';
 
-import type { MapRef } from 'react-map-gl';
+import { MapRef } from 'react-map-gl';
+import { useDispatch, useSelector, useStore } from 'react-redux';
+import setPlacesAsync from '../../rtk/async-actions';
 
 function OverviewMap() {
-  const [bounds, setBounds] = useState<LngLatBounds>();
   const mapRef = useRef<MapRef>();
-  
-  const { classes } = useStyles();
+  // const store = useStore<RootState>();
+  const dispatch = useDispatch<AppDispatch>();
+  const bounds = useSelector((state: RootState) => state.bounds);
 
-  const onLoad = () => {
-    setBounds(mapRef.current?.getBounds());
-  }
-
-  const onMoveEnd = () => {
-    setBounds(mapRef.current?.getBounds());
-  }
+  const onChange = () => {
+    dispatch(setBounds(mapRef.current?.getBounds()));
+  };
 
   useEffect(() => {
-    console.log(bounds)
-  }, [bounds])
+    const isRendered = (bounds._ne) as boolean;
+
+    if (isRendered) {
+      console.log(bounds);
+
+      // Uncomment this when you're gonna use or test the app
+
+      // dispatch<AppDispatch>(setPlacesAsync({
+      //   bl_latitude: bounds?._sw.lat,
+      //   bl_longitude: bounds?._sw.lng,
+      //   tr_longitude: bounds?._ne.lng,
+      //   tr_latitude: bounds?._ne.lat,
+      // }));
+    }
+  }, [bounds]);
 
   return (
     <Map
@@ -30,13 +42,13 @@ function OverviewMap() {
       initialViewState={{
         longitude: -122.4,
         latitude: 37.8,
-        zoom: 14
+        zoom: 14,
       }}
       mapboxAccessToken={ token }
       style={{width: '100%', height: '80vh'}}
       mapStyle="mapbox://styles/mapbox/streets-v9"
-      onLoad={ onLoad }
-      onMoveEnd={ onMoveEnd }
+      onLoad={ onChange }
+      onMoveEnd={ onChange }
     />
   )
 }
