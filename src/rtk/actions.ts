ce7d30355be9b9993aafc20bcfa6adfef750b1c4
Artem <awrer1234eads@gmail.com>
2022-07-16 14:47:39 +0300

    create setrating and settype actions

--- src/rtk/actions.ts
+++ src/rtk/actions.ts
@@ -4,9 +4,14 @@ import { LngLatBounds } from "mapbox-gl";
 const ACTION_NAMES = {
   SET_BOUNDS: 'SET_BOUNDS',
   SET_PLACES: 'SET_PLACES',
+  SET_TYPE: 'SET_TYPE',
+  SET_RATING: 'SET_RATING',
 };
 
 const setBounds = createAction<LngLatBounds | undefined>(ACTION_NAMES.SET_BOUNDS);
 const setPlaces = createAction<object>(ACTION_NAMES.SET_PLACES);
 
-export { ACTION_NAMES, setBounds, setPlaces };
+const setType = createAction<string>(ACTION_NAMES.SET_TYPE);
+const setRating = createAction<number>(ACTION_NAMES.SET_RATING);
+
+export { ACTION_NAMES, setBounds, setPlaces, setType, setRating };
