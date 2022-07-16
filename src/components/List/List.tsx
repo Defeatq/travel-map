c9a961deed5ab471c9731e2197c868a0f26c5a32
Artem <awrer1234eads@gmail.com>
2022-07-16 14:50:06 +0300

    let select set rating and type to store

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,19 +1,29 @@
-import { Box, FormControl, Grid, InputLabel, MenuItem, Select, SelectChangeEvent, Typography } from "@mui/material";
-import { useState } from "react";
+import { 
+  Box, 
+  FormControl, 
+  Grid, 
+  InputLabel, 
+  MenuItem, 
+  Select, 
+  SelectChangeEvent, 
+  Typography 
+} from "@mui/material";
 
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
-import { RootState } from "../../rtk/store";
+import { RootState, AppDispatch } from "../../rtk/store";
+import { setRating, setType } from "../../rtk/actions";
+
 import useStyles from "./ListStyle";
 import DetailsCard from '../DetailsCard/DetailsCard';
 
 function List() {
-  const [type, setType] = useState('');
-  const [rating, setRating] = useState('');
+  const dispatch = useDispatch<AppDispatch>();
+  const places = useSelector((state: RootState) => state.places);
+  const type = useSelector((state: RootState) => state.filters.type);
+  const rating = useSelector((state: RootState) => state.filters.rating);
 
   const { classes } = useStyles();
-
-  const places = useSelector((state: RootState) => state.places);
 
   return (
     <div className={ classes.details }>
@@ -23,7 +33,7 @@ function List() {
       <div className={ classes.detailsSelect }>
         <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }} className={ classes.detailsForm }>
           <InputLabel>Type</InputLabel>
-          <Select value={ type } onChange={ (event: SelectChangeEvent) => setType(event.target.value) } label="Type">
+          <Select value={ type } onChange={ (event: SelectChangeEvent) => dispatch(setType(event.target.value)) } label="Type">
             <MenuItem value={ 'restaurants' }>Restaurants</MenuItem>
             <MenuItem value={ 'hotels' }>Hotels</MenuItem>
             <MenuItem value={ 'attractions' }>Attractions</MenuItem>
@@ -31,7 +41,7 @@ function List() {
         </FormControl>
         <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }} className={ classes.detailsForm }>
           <InputLabel>Rating</InputLabel>
-          <Select value={ rating } onChange={ (event: SelectChangeEvent) => setRating(event.target.value) } label="Rating">
+          <Select value={ rating } onChange={ (event: SelectChangeEvent<number>) => dispatch(setRating(event.target.value as number)) } label="Rating">
             <MenuItem value={ 0 }>All</MenuItem>
             <MenuItem value={ 3 }>Above 3.0</MenuItem>
             <MenuItem value={ 4 }>Above 4.0</MenuItem>
