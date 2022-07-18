3a37690783bc8bdf1fc596efd8a6dcce8a24c4f3
Artem <awrer1234eads@gmail.com>
2022-07-18 18:58:01 +0300

    make uvisible inaccessible places

--- src/components/DetailsCard/DetailsCard.tsx
+++ src/components/DetailsCard/DetailsCard.tsx
@@ -26,90 +26,93 @@ import useStyles from "./DetailsCardStyle";
 
 function DetailsCard( props: CardInterface ) {
   const { place } = props;
-  const hasImage = (place.photo) as boolean;
+  const hasName = place.name as boolean;
+  const hasImage = place.photo as boolean;
 
   const { classes } = useStyles();
 
-  return (
-    <Card elevation={ 2 } sx={{ maxWidth: '100%' }}>
-      <CardMedia
-        component="img"
-        height="250px"
-        image={ hasImage ? place?.photo?.images?.large?.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg' }
-        alt=""
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5">
-          { place?.name }
-        </Typography>
-
-        <Box display="flex" justifyContent="space-between">
-          <Rating name="rating" value={ place?.rating } precision={ 0.5 } readOnly className={ classes.subtitleStart }/>
-          <Typography gutterBottom variant="subtitle1" className={ classes.subtitleEnd }>
-            { place?.num_reviews } reviews
+  if (hasName) {
+    return (
+      <Card elevation={ 2 } sx={{ maxWidth: '100%' }}>
+        <CardMedia
+          component="img"
+          height="250px"
+          image={ hasImage ? place?.photo?.images?.large?.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg' }
+          alt=""
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5">
+            { place?.name }
           </Typography>
-        </Box>
-
-        <Box display="flex" justifyContent="space-between">
-          <Typography variant="subtitle1" className={ classes.subtitleStart }>
-            Price
-          </Typography>
-          <Typography gutterBottom variant="subtitle1" className={ classes.subtitleEnd }>
-            { place?.price }
-          </Typography>
-        </Box>
-
-        <Box display="flex" justifyContent="space-between">
-          <Typography variant="subtitle1" className={ classes.subtitleStart }>
-            Ranking
-          </Typography>
-          <Typography gutterBottom variant="subtitle1" className={ classes.subtitleEnd }>
-            { place?.ranking }
-          </Typography>
-        </Box>
-
-        {
-          place?.awards?.map((award: { images: { small: string | undefined; }; display_name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; }, i: any) => 
-            <Box key={ i } display="flex" justifyContent="space-between">
-              <img src={ award?.images.small } alt="" className={ classes.iconReward }/>
-              <Typography gutterBottom variant="subtitle1" className={ classes.subtitleEnd }>
-                { award?.display_name }
-              </Typography>
-            </Box>
-          )
-        }
-
-        {
-          place?.cuisine?.map((currentCuisine: { name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; }, i: Key | null | undefined) =>
-            <Chip key={ i } label={ currentCuisine?.name } className={ classes.chip }/> 
-          )
-        }
-
-        <Divider sx={{ marginBottom: '5px' }}/>
-
-        <Box display="flex" justifyContent="space-between">
-          <Typography variant="subtitle1" className={ classes.subtitleStart }>
-            <PlaceIcon />
-          </Typography>
-          <Typography gutterBottom variant="subtitle1" className={ classes.subtitleEnd }>
-            { place?.address }
-          </Typography>
-        </Box>
-        <Box display="flex" justifyContent="space-between">
-          <Typography variant="subtitle1" className={ classes.subtitleStart }>
-            <PhoneIcon />
-          </Typography>
-          <Typography gutterBottom variant="subtitle1" className={ classes.subtitleEnd }>
-            { place?.phone }
-          </Typography>
-        </Box>
-      </CardContent>
-      <CardActions>
-        <Button size="small" onClick={ () => window.open(place?.web_url, '_blank') }>Trip advisor</Button>
-        <Button size="small" onClick={ () => window.open(place?.website, '_blank')}>Website</Button>
-      </CardActions>
-    </Card>
-  )
+  
+          <Box display="flex" justifyContent="space-between">
+            <Rating name="rating" value={ place?.rating } precision={ 0.5 } readOnly className={ classes.subtitleStart }/>
+            <Typography gutterBottom variant="subtitle1" className={ classes.subtitleEnd }>
+              { place?.num_reviews } reviews
+            </Typography>
+          </Box>
+  
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="subtitle1" className={ classes.subtitleStart }>
+              Price
+            </Typography>
+            <Typography gutterBottom variant="subtitle1" className={ classes.subtitleEnd }>
+              { place?.price }
+            </Typography>
+          </Box>
+  
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="subtitle1" className={ classes.subtitleStart }>
+              Ranking
+            </Typography>
+            <Typography gutterBottom variant="subtitle1" className={ classes.subtitleEnd }>
+              { place?.ranking }
+            </Typography>
+          </Box>
+  
+          {
+            place?.awards?.map((award: { images: { small: string | undefined; }; display_name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; }, i: any) => 
+              <Box key={ i } display="flex" justifyContent="space-between">
+                <img src={ award?.images.small } alt="" className={ classes.iconReward }/>
+                <Typography gutterBottom variant="subtitle1" className={ classes.subtitleEnd }>
+                  { award?.display_name }
+                </Typography>
+              </Box>
+            )
+          }
+  
+          {
+            place?.cuisine?.map((currentCuisine: { name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; }, i: Key | null | undefined) =>
+              <Chip key={ i } label={ currentCuisine?.name } className={ classes.chip }/> 
+            )
+          }
+  
+          <Divider sx={{ marginBottom: '5px' }}/>
+  
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="subtitle1" className={ classes.subtitleStart }>
+              <PlaceIcon />
+            </Typography>
+            <Typography gutterBottom variant="subtitle1" className={ classes.subtitleEnd }>
+              { place?.address }
+            </Typography>
+          </Box>
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="subtitle1" className={ classes.subtitleStart }>
+              <PhoneIcon />
+            </Typography>
+            <Typography gutterBottom variant="subtitle1" className={ classes.subtitleEnd }>
+              { place?.phone }
+            </Typography>
+          </Box>
+        </CardContent>
+        <CardActions>
+          <Button size="small" onClick={ () => window.open(place?.web_url, '_blank') }>Trip advisor</Button>
+          <Button size="small" onClick={ () => window.open(place?.website, '_blank')}>Website</Button>
+        </CardActions>
+      </Card>
+    )
+  }
 }
 
 export default DetailsCard;
