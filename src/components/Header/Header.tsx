41f36ff4023b7fd3f3f14c28fb3832c4f69ae2ab
Artem <awrer1234eads@gmail.com>
2022-07-20 19:20:29 +0300

    create autocomplete around input

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,36 @@
-import { AppBar, Autocomplete, Box, InputAdornment, TextField, Toolbar, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { 
+  AppBar, 
+  Autocomplete, 
+  Box, 
+  TextField, 
+  Toolbar, 
+  Typography 
+} from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+
 import useStyles from './HeaderStyle';
+import { setAutocompleteResultsAsync } from '../../rtk/async-actions';
+import { RootState, AppDispatch } from '../../rtk/store';
 
 function Header() {
+  const [value, setValue] = useState<string>('');
+  const autoCompleteResults = useSelector((state: RootState) => state.autocomplete.results);
+  const dispatch = useDispatch<AppDispatch>();
+
   const { classes } = useStyles();
+
+  useEffect(() => {
+    // uncomment this when you're gonna use or test the app
+
+    // dispatch(setAutocompleteResultsAsync(value));
+  }, [value]);
+
+  useEffect(() => {
+    console.log(autoCompleteResults);
+  }, [autoCompleteResults]);
 
   return (
     <AppBar position="static">
@@ -15,17 +42,37 @@ function Header() {
           <Typography variant="h6" className={ classes.title }>
             Explore new places
           </Typography>
-          <div className={ classes.search }>
-            <div className={ classes.searchIcon }>
-              <SearchIcon />
-            </div>
-            <TextField placeholder="Search..." label="" variant="outlined" 
-                      InputProps={{ 
-                        className: classes.searchInput, 
-                        classes: { 
-                          focused: classes.inputFocused,
-                        } }}/>
-          </div>
+          <Autocomplete
+            freeSolo
+            disableClearable
+            options={ 
+              autoCompleteResults.map((option) => 
+                option.text ? option.text : option?.detailsV2?.names?.name
+              ) 
+            }
+            renderInput={ (params) => (
+              <div className={ classes.search }>
+                <div className={ classes.searchIcon }>
+                  <SearchIcon />
+                </div>
+                <TextField
+                  { ...params }
+                  value={ value }
+                  onChange={ (event) => setValue(event.target.value) }
+                  placeholder="Search..." 
+                  label="" 
+                  variant="outlined" 
+                  InputProps={{
+                    ...params.InputProps,
+                    type: 'search',
+                    className: classes.searchInput, 
+                    classes: { 
+                      focused: classes.inputFocused,
+                    } }}
+                />
+              </div>
+            ) }
+          />
         </Box>
       </Toolbar>
     </AppBar>
