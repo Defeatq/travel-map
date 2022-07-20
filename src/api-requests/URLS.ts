fdf9b1f848c4b6bb3ac001b68b0fddd03dd23697
Artem <awrer1234eads@gmail.com>
2022-07-20 19:15:55 +0300

    create new url generator for autocomplete

--- src/api-requests/URLS.ts
+++ src/api-requests/URLS.ts
@@ -20,8 +20,11 @@ interface Params {
   // adults?: number,
 }
 
-function getUrlBoundsList (params: Params) {
-  return `https://travel-advisor.p.rapidapi.com/${ store.getState().filters.type }/list-in-boundary?bl_latitude=${ params.bl_latitude }&bl_longitude=${ params.bl_longitude }&tr_longitude=${ params.tr_longitude }&tr_latitude=${ params.tr_latitude }`
-}
+const getUrlBoundsList = (params: Params) => `https://travel-advisor.p.rapidapi.com/${ store.getState().filters.type }/list-in-boundary?bl_latitude=${ params.bl_latitude }&bl_longitude=${ params.bl_longitude }&tr_longitude=${ params.tr_longitude }&tr_latitude=${ params.tr_latitude }`;
 
-export default getUrlBoundsList;
+const getAutoCompleteUrl = (text: string) => `https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete?query=${ encodeURIComponent(text) }&lang=en_US&units=km`;
+
+export {
+  getUrlBoundsList,
+  getAutoCompleteUrl,
+}
